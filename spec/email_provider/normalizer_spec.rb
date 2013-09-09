ab8c0263162a9548795938a522bd825c5f26043e
Eric J. Holmes <eric@ejholmes.net>
2013-09-09 12:32:17 -0700

    Check all mappings.

--- spec/email_provider/normalizer_spec.rb
+++ spec/email_provider/normalizer_spec.rb
@@ -1,14 +1,15 @@
 require 'spec_helper'
 
 describe EmailProvider::Normalizer do
+  MAPPINGS = YAML.load File.read(File.expand_path('../../../data/mappings.yml', __FILE__))
+
   subject(:normalizer) { described_class.new provider }
 
   describe '#normalize' do
+    let(:mappings) { YAML.load File.read(File.expand_path('../../../data/mappings.yml', __FILE__)) }
     subject { normalizer.normalize }
 
-    [
-      [:googlemail, :gmail]
-    ].each do |(input, output)|
+    MAPPINGS.each do |input, output|
       context "when the input is #{input}" do
         let(:provider) { input }
         it { should eq output }
--- spec/email_provider_spec.rb
+++ spec/email_provider_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 
 describe EmailProvider do
   describe '.find' do
-    it do
-      described_class.find('eric@ejholmes.net')
+    it 'delegates to Finder.find' do
+      EmailProvider::Finder.should_receive(:find).with('eric@remind101.com')
+      described_class.find('eric@remind101.com')
     end
   end
 end
